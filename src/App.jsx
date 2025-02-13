e6cd07f97400d264a38fb2e62bad29e9c481e6c2
Abdalaziz Mohamed <124760094+Abdalaziz0486m@users.noreply.github.com>
2025-02-13 13:59:46 +0200

    Add important libraries and init components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Routes>
+          <Route path="" elemen />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
